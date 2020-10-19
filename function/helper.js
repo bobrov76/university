a0a489b0f92873c98a29fdc637f43ac89262b1e5
bobrov76 <50507472+bobrov76@users.noreply.github.com>
2020-10-19 14:42:49 +0700

    fix isEvens

--- function/helper.js
+++ function/helper.js
@@ -2,7 +2,7 @@ isEvens = () => {
   let now = new Date();
   let onejan = new Date(now.getFullYear(), 0, 1);
   let week = Math.ceil( (((now - onejan) / 86400000) + onejan.getDay() + 1) / 7 );
-  return week-1 % 2 ? 1 : 0;
+  return week-1 % 2 == 0 ? 1 : 0;
 };
 
 getToday = () => {
